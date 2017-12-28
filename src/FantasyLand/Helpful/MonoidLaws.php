47eebda37458db7a0aece6783b2eb15fc9ee2b79
widmogrod <widmogrod@gmail.com>
2017-12-28 23:34:39 +0100

    fix MonoidLaws check style
    fix MonoidLaws check style

--- src/FantasyLand/Helpful/MonoidLaws.php
+++ src/FantasyLand/Helpful/MonoidLaws.php
@@ -12,17 +12,16 @@ class MonoidLaws
      * Generic test to verify if a type obey the monodic laws.
      *
      * @param callable $assertEqual Asserting function (Monoid $m1, Monoid $m2, $message)
-     * @param Monoid $x
-     * @param Monoid $y
-     * @param Monoid $z
+     * @param Monoid   $x
+     * @param Monoid   $y
+     * @param Monoid   $z
      */
     public static function test(
         callable $assertEqual,
         Monoid $x,
         Monoid $y,
         Monoid $z
-    )
-    {
+    ) {
         $assertEqual(
             $x->concat($x::mempty()),
             $x,
