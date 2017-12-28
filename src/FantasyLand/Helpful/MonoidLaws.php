a986833df301c2da11ecd76eed6af5fd4d218bbf
widmogrod <widmogrod@gmail.com>
2017-12-28 23:30:31 +0100

    MonoidLaws + tests

---
+++ src/FantasyLand/Helpful/MonoidLaws.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FunctionalPHP\FantasyLand\Helpful;
+
+use FunctionalPHP\FantasyLand\Monoid;
+
+class MonoidLaws
+{
+    /**
+     * Generic test to verify if a type obey the monodic laws.
+     *
+     * @param callable $assertEqual Asserting function (Monoid $m1, Monoid $m2, $message)
+     * @param Monoid $x
+     * @param Monoid $y
+     * @param Monoid $z
+     */
+    public static function test(
+        callable $assertEqual,
+        Monoid $x,
+        Monoid $y,
+        Monoid $z
+    )
+    {
+        $assertEqual(
+            $x->concat($x::mempty()),
+            $x,
+            'Right identity'
+        );
+
+        $assertEqual(
+            $x::mempty()->concat($x),
+            $x,
+            'Left identity'
+        );
+
+        $assertEqual(
+            $x->concat($y->concat($z)),
+            $x->concat($y)->concat($z),
+            'Associativity'
+        );
+    }
+}
---
+++ src/FantasyLand/Helpful/Tests/MonoidLawsTest.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FunctionalPHP\FantasyLand\Helpful\Tests;
+
+use Eris\Generator;
+use Eris\TestTrait;
+use FunctionalPHP\FantasyLand\Helpful\MonoidLaws;
+use FunctionalPHP\FantasyLand\Monoid;
+use FunctionalPHP\FantasyLand\Semigroup;
+
+class StringMonoid implements Monoid
+{
+    /**
+     * @var string
+     */
+    private $value;
+
+    public function __construct(string $value)
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public static function mempty()
+    {
+        return new self("");
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function concat(Semigroup $value): Semigroup
+    {
+        return new self($this->value . $value->value);
+    }
+}
+
+class MonoidLawsTest extends \PHPUnit_Framework_TestCase
+{
+    use TestTrait;
+
+    public function test_it_should_obay_monoid_laws()
+    {
+        $this->forAll(
+            Generator\string(),
+            Generator\string(),
+            Generator\string()
+        )->then(function (string $a, string $b, string $c) {
+            MonoidLaws::test(
+                [$this, 'assertEquals'],
+                new StringMonoid($a),
+                new StringMonoid($b),
+                new StringMonoid($c)
+            );
+        });
+    }
+}
