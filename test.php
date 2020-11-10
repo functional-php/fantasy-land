9e5687e8b8f246dbc18941e262db92700c69d282
Thomas Vargiu <tvargiu@gmail.com>
2020-11-10 03:59:22 +0100

    Enabled tests for previous PHP versions

--- test.php
+++ test.php
@@ -1,8 +1,9 @@
 <?php
 
+declare(strict_types=1);
+
 require __DIR__ . '/vendor/autoload.php';
 
-use FunctionalPHP\FantasyLand\Useful\Identity;
 use FunctionalPHP\FantasyLand as f;
 
 $f = function (int $a): int {
