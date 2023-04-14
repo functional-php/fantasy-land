e2c61bc65d3f892a0606e437ea1ab4d5fa7221b2
Andrew Moyes <andrew.moyes@futurenet.com>
2023-04-14 11:17:28 +0200

    Typo fix in docblock

--- src/FantasyLand/Useful/Identity.php
+++ src/FantasyLand/Useful/Identity.php
@@ -33,7 +33,7 @@ class Identity implements FantasyLand\Monad
     }
 
     /**
-     * @inheritdocus
+     * @inheritdoc
      */
     public function map(callable $transformation): FantasyLand\Functor
     {
